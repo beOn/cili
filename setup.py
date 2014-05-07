b943b4c618a37cbd753ecf262f701323ff57b5e9
Ben Acland <benacland@gmail.com>
2014-05-07 14:08:32 -0500

    better readme parsing for pypi

--- setup.py
+++ setup.py
@@ -3,14 +3,20 @@
 import os
 from setuptools import setup
 
-def read(fname):
-    fpath = os.path.join(os.path.dirname(__file__), fname)
-    if os.path.exists(fpath):
-      return open(fpath).read()
-    return ""
+def get_readme():
+    md_path = os.path.join(os.path.dirname(__file__), "README.md")
+    txt_path = os.path.join(os.path.dirname(__file__), "README.txt")
+
+    if os.path.exists(txt_path):
+        d = open(txt_path).read()
+    elif os.path.exists(md_path):
+        d = open(md_path).read()
+    else:
+        d = ""
+    return d
 
 setup(name='cili',
-      version='0.5.0',
+      version='0.5.1',
       author='Ben Acland',
       author_email='benacland@gmail.com',
       description='Eyetracking data tools based on pandas',
@@ -19,7 +25,7 @@ setup(name='cili',
       url='https://github.com/beOn/cili',
       install_requires=['scipy','numexpr','tables','pandas'],
       packages=['cili'],
-      long_description=read('README.md'),
+      long_description=get_readme(),
       classifiers=[
         'Development Status :: 2 - Pre-Alpha',
         'Topic :: Scientific/Engineering :: Information Analysis',
