b7856edbd03326f13f1841dc81dfdf36522611ef
tomwallis <tsawallis@gmail.com>
2015-03-11 13:00:20 +0100

    renamed test data files

--- tests/test_extract.py
+++ tests/test_extract.py
@@ -14,10 +14,10 @@ import os
 def test_extract_events_1():
     """Test event extraction for dataset 2, "duration" of
     5 samples"""
-    fname = os.path.join(os.getcwd(), 'tests', 'data_2.asc')
+    fname = os.path.join(os.getcwd(), 'tests', 'binoc_250Hz_2.asc')
     samps, events = load_eyelink_dataset(fname)
     extracted = extract_events(samps, events.EFIX, duration=5)
 
-    fname = os.path.join(os.getcwd(), 'tests', 'extracted_1.csv')
+    fname = os.path.join(os.getcwd(), 'tests', 'binoc_250Hz_2_extracted.csv')
     true = pd.read_csv(fname, index_col=[0, 1])
     assert_frame_equal(extracted, true)
--- tests/test_util.py
+++ tests/test_util.py
@@ -15,39 +15,39 @@ import os
 
 def test_load_eyelink_dataset_1():
     """Test loading samples..."""
-    fname = os.path.join(os.getcwd(), 'tests', 'data_1.asc')
+    fname = os.path.join(os.getcwd(), 'tests', 'binoc_250Hz_1.asc')
     samps, events = load_eyelink_dataset(fname)
 
-    fname = os.path.join(os.getcwd(), 'tests', 'samps_1.csv')
+    fname = os.path.join(os.getcwd(), 'tests', 'binoc_250Hz_1_samps.csv')
     true = pd.read_csv(fname, index_col='onset')
     assert_frame_equal(samps, true)
 
 
 def test_load_eyelink_dataset_2():
     """Test loading events..."""
-    fname = os.path.join(os.getcwd(), 'tests', 'data_1.asc')
+    fname = os.path.join(os.getcwd(), 'tests', 'binoc_250Hz_1.asc')
     samps, events = load_eyelink_dataset(fname)
 
-    fname = os.path.join(os.getcwd(), 'tests', 'events_1.csv')
+    fname = os.path.join(os.getcwd(), 'tests', 'binoc_250Hz_1_events.csv')
     true = pd.read_csv(fname, index_col=0)
     assert_frame_equal(events.EFIX, true)
 
 
 def test_load_eyelink_dataset_3():
     """Test loading samples..."""
-    fname = os.path.join(os.getcwd(), 'tests', 'data_2.asc')
+    fname = os.path.join(os.getcwd(), 'tests', 'binoc_250Hz_2.asc')
     samps, events = load_eyelink_dataset(fname)
 
-    fname = os.path.join(os.getcwd(), 'tests', 'samps_2.csv')
+    fname = os.path.join(os.getcwd(), 'tests', 'binoc_250Hz_2_samps.csv')
     true = pd.read_csv(fname, index_col='onset')
     assert_frame_equal(samps, true)
 
 
 def test_load_eyelink_dataset_4():
     """Test loading events..."""
-    fname = os.path.join(os.getcwd(), 'tests', 'data_2.asc')
+    fname = os.path.join(os.getcwd(), 'tests', 'binoc_250Hz_2.asc')
     samps, events = load_eyelink_dataset(fname)
 
-    fname = os.path.join(os.getcwd(), 'tests', 'events_2.csv')
+    fname = os.path.join(os.getcwd(), 'tests', 'binoc_250Hz_2_events.csv')
     true = pd.read_csv(fname, index_col=0)
     assert_frame_equal(events.EFIX, true)
