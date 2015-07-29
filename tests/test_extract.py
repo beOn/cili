6d4d62f077c1c9e3582104394cac6d718a2f8948
Ben Acland <benacland@gmail.com>
2015-07-28 22:26:28 -0500

    finish off extraction tests

--- tests/test_extract.py
+++ tests/test_extract.py
@@ -2,6 +2,7 @@ import os
 import numpy as np
 import pandas as pd
 from nose.tools import assert_equal, assert_true, assert_raises, raises
+from numpy.testing import assert_array_equal
 
 from config import *
 from cili.extract import *
@@ -94,12 +95,99 @@ def test_negative_duration():
     ext = extract_events(ds, fixations, duration=-1000, units=TIME_UNITS)
 
 def test_posneg_offset():
+    # make sure that you can use a positive or a negative offset
     ds, es = pandas_dfs_from_asc(paths['mono250'])
     fixations = es.EFIX[3:-1]
     ext = extract_events(ds, fixations, duration=500, offset=-250, units=TIME_UNITS)
     ext = extract_events(ds, fixations, duration=500, offset=250, units=TIME_UNITS)
     assert(True)
 
-# TODO: test fields of returned data for all data types
+""" fields """
 
-# TODO: test that you can borrow attributes
+def test_extracted__bino250_cols():
+    ds, es = pandas_dfs_from_asc(paths['bino250'])
+    rs = es.ESACC[:-3]
+    ext = extract_events(ds, rs, duration=500, offset=0, units=TIME_UNITS)
+    test_cols = ['x_l', 'y_l', 'pup_l', 'x_r', 'y_r', 'pup_r', 'samp_warns', 'orig_idx']
+    assert_array_equal(test_cols, ds.columns.tolist())
+
+def test_extracted_bino500_cols():
+    ds, es = pandas_dfs_from_asc(paths['bino500'])
+    rs = es.ESACC[:-3]
+    ext = extract_events(ds, rs, duration=500, offset=0, units=TIME_UNITS)
+    test_cols = ['x_l', 'y_l', 'pup_l', 'x_r', 'y_r', 'pup_r', 'samp_warns', 'orig_idx']
+    assert_array_equal(test_cols, ds.columns.tolist())
+
+def test_extracted_bino1000_cols():
+    ds, es = pandas_dfs_from_asc(paths['bino1000'])
+    rs = es.ESACC[:-3]
+    ext = extract_events(ds, rs, duration=500, offset=0, units=TIME_UNITS)
+    test_cols = ['x_l', 'y_l', 'pup_l', 'x_r', 'y_r', 'pup_r', 'samp_warns', 'orig_idx']
+    assert_array_equal(test_cols, ds.columns.tolist())
+
+def test_extracted_binoRemote250_cols():
+    ds, es = pandas_dfs_from_asc(paths['binoRemote250'])
+    rs = es.EFIX[:-3]
+    ext = extract_events(ds, rs, duration=500, offset=0, units=TIME_UNITS)
+    test_cols = ['x_l', 'y_l', 'pup_l', 'x_r', 'y_r', 'pup_r', 'samp_warns', 'targ_x',
+        'targ_y', 'targ_dist', 'remote_warns', 'orig_idx']
+    assert_array_equal(test_cols, ds.columns.tolist())
+
+def test_extracted_binoRemote500_cols():
+    ds, es = pandas_dfs_from_asc(paths['binoRemote500'])
+    rs = es.ESACC[:-3]
+    ext = extract_events(ds, rs, duration=500, offset=0, units=TIME_UNITS)
+    test_cols = ['x_l', 'y_l', 'pup_l', 'x_r', 'y_r', 'pup_r', 'samp_warns', 'targ_x',
+        'targ_y', 'targ_dist', 'remote_warns', 'orig_idx']
+    assert_array_equal(test_cols, ds.columns.tolist())
+
+def test_extracted_mono250_cols():
+    ds, es = pandas_dfs_from_asc(paths['mono250'])
+    rs = es.ESACC[:-3]
+    ext = extract_events(ds, rs, duration=500, offset=0, units=TIME_UNITS)
+    test_cols = ['x_l', 'y_l', 'pup_l', 'samp_warns', 'orig_idx']
+    assert_array_equal(test_cols, ds.columns.tolist())
+
+def test_extracted_mono500_cols():
+    ds, es = pandas_dfs_from_asc(paths['mono500'])
+    rs = es.ESACC[:-3]
+    ext = extract_events(ds, rs, duration=500, offset=0, units=TIME_UNITS)
+    test_cols = ['x_l', 'y_l', 'pup_l', 'samp_warns', 'orig_idx']
+    assert_array_equal(test_cols, ds.columns.tolist())
+
+def test_extracted_mono1000_cols():
+    ds, es = pandas_dfs_from_asc(paths['mono1000'])
+    rs = es.ESACC[:-3]
+    ext = extract_events(ds, rs, duration=500, offset=0, units=TIME_UNITS)
+    test_cols = ['x_r', 'y_r', 'pup_r', 'samp_warns', 'orig_idx']
+    assert_array_equal(test_cols, ds.columns.tolist())
+
+def test_extracted_mono2000_cols():
+    ds, es = pandas_dfs_from_asc(paths['mono2000'])
+    rs = es.ESACC[:-3]
+    ext = extract_events(ds, rs, duration=500, offset=0, units=TIME_UNITS)
+    test_cols = ['x_r', 'y_r', 'pup_r', 'samp_warns', 'orig_idx']
+    assert_array_equal(test_cols, ds.columns.tolist())
+
+def test_extracted_monoRemote250_cols():
+    ds, es = pandas_dfs_from_asc(paths['monoRemote250'])
+    rs = es.EFIX[:-3]
+    ext = extract_events(ds, rs, duration=500, offset=0, units=TIME_UNITS)
+    test_cols = ['x_l', 'y_l', 'pup_l', 'samp_warns', 'targ_x', 'targ_y', 'targ_dist',
+        'remote_warns', 'orig_idx']
+    assert_array_equal(test_cols, ds.columns.tolist())
+
+def test_extracted_monoRemote500_cols():
+    ds, es = pandas_dfs_from_asc(paths['monoRemote500'])
+    rs = es.ESACC[:-3]
+    ext = extract_events(ds, rs, duration=500, offset=0, units=TIME_UNITS)
+    test_cols = ['x_l', 'y_l', 'pup_l', 'samp_warns', 'targ_x', 'targ_y', 'targ_dist',
+        'remote_warns', 'orig_idx']
+    assert_array_equal(test_cols, ds.columns.tolist())
+
+def test_borrowed_fields():
+    ds, es = pandas_dfs_from_asc(paths['binoRemote500'])
+    rs = es.ESACC[:-3]
+    ext = extract_events(ds, rs, duration=500, offset=0, units=TIME_UNITS,
+        borrow_attributes=['peak_velocity'])
+    assert_true('peak_velocity' in ext.columns)
