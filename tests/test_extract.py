a5b89a109c52d56f075cb7778afa11301a1c8e3f
Ben Acland <benacland@gmail.com>
2018-07-06 13:02:02 -0500

    fix some more tests

--- tests/test_extract.py
+++ tests/test_extract.py
@@ -101,9 +101,9 @@ def test_posneg_offset():
     # make sure that you can use a positive or a negative offset
     ds, es = pandas_dfs_from_asc(paths['mono250'])
     fixations = es.EFIX[3:-1]
-    ext = extract_events(ds, fixations, duration=500,
+    ext = extract_events(ds, fixations, duration=40,
                          offset=-250, units=TIME_UNITS)
-    ext = extract_events(ds, fixations, duration=500,
+    ext = extract_events(ds, fixations, duration=40,
                          offset=250, units=TIME_UNITS)
     assert(True)
 
@@ -114,7 +114,7 @@ def test_posneg_offset():
 def test_extracted__bino250_cols():
     ds, es = pandas_dfs_from_asc(paths['bino250'])
     rs = es.ESACC[:-3]
-    ext = extract_events(ds, rs, duration=500, offset=0, units=TIME_UNITS)
+    ext = extract_events(ds, rs, duration=40, offset=0, units=TIME_UNITS)
     test_cols = ['x_l', 'y_l', 'pup_l', 'x_r',
                  'y_r', 'pup_r', 'samp_warns', 'orig_idx']
     assert_array_equal(test_cols, ds.columns.tolist())
@@ -141,7 +141,7 @@ def test_extracted_bino1000_cols():
 def test_extracted_binoRemote250_cols():
     ds, es = pandas_dfs_from_asc(paths['binoRemote250'])
     rs = es.EFIX[:-3]
-    ext = extract_events(ds, rs, duration=500, offset=0, units=TIME_UNITS)
+    ext = extract_events(ds, rs, duration=40, offset=0, units=TIME_UNITS)
     test_cols = ['x_l', 'y_l', 'pup_l', 'x_r', 'y_r', 'pup_r', 'samp_warns', 'targ_x',
                  'targ_y', 'targ_dist', 'remote_warns', 'orig_idx']
     assert_array_equal(test_cols, ds.columns.tolist())
@@ -159,7 +159,7 @@ def test_extracted_binoRemote500_cols():
 def test_extracted_mono250_cols():
     ds, es = pandas_dfs_from_asc(paths['mono250'])
     rs = es.ESACC[:-3]
-    ext = extract_events(ds, rs, duration=500, offset=0, units=TIME_UNITS)
+    ext = extract_events(ds, rs, duration=40, offset=0, units=TIME_UNITS)
     test_cols = ['x_l', 'y_l', 'pup_l', 'samp_warns', 'orig_idx']
     assert_array_equal(test_cols, ds.columns.tolist())
 
@@ -175,7 +175,7 @@ def test_extracted_mono500_cols():
 def test_extracted_mono1000_cols():
     ds, es = pandas_dfs_from_asc(paths['mono1000'])
     rs = es.ESACC[:-3]
-    ext = extract_events(ds, rs, duration=500, offset=0, units=TIME_UNITS)
+    ext = extract_events(ds, rs, duration=50, offset=0, units=TIME_UNITS)
     test_cols = ['x_r', 'y_r', 'pup_r', 'samp_warns', 'orig_idx']
     assert_array_equal(test_cols, ds.columns.tolist())
 
